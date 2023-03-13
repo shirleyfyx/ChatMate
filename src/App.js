399f33a7ab4dfc89d569dd4606fb0ee85ac2eb12
Shirley Fang <fangyixuan0123@gmail.com>
2023-03-13 15:38:58 -0400

    set up firebase

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useCollectionData } from 'react-firebase-hooks/firestore';
+
+firebase.initializeApp({
+  apiKey: "AIzaSyCXfjjROq6Ft2xjzx5PVSHr93uo1UMC-p0",
+  authDomain: "chatmate-aab26.firebaseapp.com",
+  projectId: "chatmate-aab26",
+  storageBucket: "chatmate-aab26.appspot.com",
+  messagingSenderId: "856607855518",
+  appId: "1:856607855518:web:1ee36885ac9a591600fc8f",
+  measurementId: "G-VKQDYQF6RN"
+})
+
+const auth = firebase.auth();
+const firestore = firebase.firestore();
+
 function App() {
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       
       </header>
+
     </div>
   );
 }
